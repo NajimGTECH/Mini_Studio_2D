fe00b476916191a192137f62c4c9fec02c1fd19e
NajimGTECH <nbakkali@gaming.tech>
2025-03-10 11:53:08 +0100

    SFML Downloaded

--- Mini_Studio_2D/main.cpp
+++ Mini_Studio_2D/main.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Mini Studio 2D");
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Mini Studio 2D");
 
 	while (window.isOpen()) {
-		
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
 	}
 }
