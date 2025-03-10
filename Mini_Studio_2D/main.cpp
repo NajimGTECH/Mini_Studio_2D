b5479b7c86734ebcabe243dd9239ac09b2a51abe
Sacha Brossard <sacha.brossard22@gmail.com>
2025-03-10 11:22:44 +0100

    Window setup

--- Mini_Studio_2D/main.cpp
+++ Mini_Studio_2D/main.cpp
@@ -5,6 +5,14 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Mini Studio 2D");
 
 	while (window.isOpen()) {
-		
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+				std::cout << "Window closed" << std::endl;
+			}
+		}
+		window.clear();
+		window.display();
 	}
 }
