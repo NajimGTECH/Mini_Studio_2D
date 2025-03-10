f9c9d8d693ba2ca7aa8a493c82a5a83df99f432d
Mishy0485 <ayite.julia06@gmail.com>
2025-03-10 16:04:59 +0100

    ADD / feature of lamp (draw and update)

--- Mini_Studio_2D/lamp.cpp
+++ Mini_Studio_2D/lamp.cpp
@@ -1,1 +1,17 @@
 #include "lamp.h"
+
+Lamp::Lamp(int s, int h) : Entity(s, h) {
+	m_shape.setSize(sf::Vector2f(50, 70));
+	m_shape.setFillColor(sf::Color::Yellow);
+}
+
+void Lamp::update() {
+}
+
+void Lamp::draw(sf::RenderWindow& window) {
+	window.draw(m_shape);
+}
+
+void Lamp::illuminate() {
+	
+}
--- Mini_Studio_2D/lamp.h
+++ Mini_Studio_2D/lamp.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "entity.h"
 class Lamp : public Entity{
+public:
+	Lamp(int s, int h);
+	void update();
+	void draw(sf::RenderWindow& window);
+	void illuminate();
 };
 
