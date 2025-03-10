a9a2fd9fd3e5dfdf45c0583787c102b065b55b1b
NajimGTECH <nbakkali@gaming.tech>
2025-03-10 12:22:39 +0100

    Validation

--- Mini_Studio_2D/main.cpp
+++ Mini_Studio_2D/main.cpp
@@ -3,18 +3,19 @@
 #include "player.h"
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Mini Studio 2D");
+	sf::RenderWindow window(sf::VideoMode({ 800.f, 600.f }), "Mini Studio 2D");
 
-	Player player(100, 10);
+	Player player({ 100.f, 10.f });
 
 	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event)) {
+		while (window.pollEvent(event))
+		{
 			if (event.type == sf::Event::Closed) {
 				window.close();
-				std::cout << "Window closed" << std::endl;
 			}
 		}
+
 		window.clear();
 		player.draw(window);
 		window.display();
