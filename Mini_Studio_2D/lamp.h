35cf47354ed710b08e40129491104bed6fb96114
Mishy0485 <ayite.julia06@gmail.com>
2025-03-12 11:10:25 +0100

    ADD / class lamp and raycaster with the modification to be done

--- Mini_Studio_2D/lamp.h
+++ Mini_Studio_2D/lamp.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "entity.h"
 class Lamp : public Entity{
+public:
+	Lamp(int s, int h);
+	void update(float deltaTime) override;
+	void draw(sf::RenderWindow& window) override;
+	void illuminate();
 };
 
---
+++ Mini_Studio_2D/raycaster.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "window.h" // a voir si je garde
+#include "entity.h"
+#include <cmath>
+
+class Raycaster {
+
+    public:
+        Raycaster() {};
+        std::shared_ptr<Entity> attachedEntity;
+        std::vector<sf::Vector2f> intersections;
+
+        void renderRay(Grid& grid); // suremnet pas besoin d'argument : utile que pour collisions
+        float degToRad(float degree);
+    };
+};
+
